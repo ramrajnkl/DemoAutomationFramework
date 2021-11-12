cd131df7d847354b67f13e0077cb1ea634c87ac0
ramrajnkl <getramraj@outlook.com>
2021-11-12 21:50:40 +0530

    Read common values from properties file

--- SeleniumDemoFramework/src/test/java/com/demoframework/testCases/BaseClass.java
+++ SeleniumDemoFramework/src/test/java/com/demoframework/testCases/BaseClass.java
@@ -7,22 +7,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import com.demoframework.utilities.ReadConfig;
+
 public class BaseClass {
 	
-	public String baseURL="http://demo.guru99.com/V1/index.php";
-	public String username = "mngr366098";
-	public String password = "Aguhame";
+	ReadConfig readconfig = new ReadConfig();
+	
+	public String baseURL = readconfig.getApplicationURL();
+	public String username = readconfig.getusername();
+	public String password = readconfig.getpassword();
+			
 	public static WebDriver driver;
 	public  static Logger logger;
 	
 	@BeforeClass
 	public void setup()
 	{	
-		System.setProperty("webdriver.chrome.driver",
-				 System.getProperty("user.dir") +"\\Drivers\\chromedriver.exe");
 		
+		System.setProperty("webdriver.chrome.driver",  readconfig.getchromepath());
 		driver=new ChromeDriver();
-		
 		
 		logger = Logger.getLogger("SeleniumDemoFramework");
 		PropertyConfigurator.configure("log4j.properties");
--- SeleniumDemoFramework/src/test/java/com/demoframework/utilities/ReadConfig.java
+++ SeleniumDemoFramework/src/test/java/com/demoframework/utilities/ReadConfig.java
@@ -1,5 +1,7 @@
 package com.demoframework.utilities;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Properties;
 
 public class ReadConfig {
@@ -8,7 +10,58 @@ public class ReadConfig {
 	
 	public ReadConfig()
 	{
+		File src = new File("./Configuration/config.properties");
 		
+		try
+		{
+			FileInputStream fis = new FileInputStream(src);
+			pro = new Properties();
+			pro.load(fis);
+			
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Exception is " + ex.getMessage());
+		}
+		
+		
+	}
+	
+	
+	public String getApplicationURL()
+	{
+		String url = pro.getProperty("baseURL"); 
+		return url;
+	}
+	
+	public String getusername()
+	{
+		String username = pro.getProperty("username"); 
+		return username;
+	}
+	
+	public String getpassword()
+	{
+		String password = pro.getProperty("password"); 
+		return password;
+	}
+	
+	public String getchromepath()
+	{
+		String chromepath = pro.getProperty("chromepath"); 
+		return chromepath;
+	}
+	
+	public String getiepath()
+	{
+		String iepath = pro.getProperty("iepath"); 
+		return iepath;
+	}
+	
+	public String getfirefoxpath()
+	{
+		String firefoxpath = pro.getProperty("firefoxpath"); 
+		return firefoxpath;
 	}
 
 }
