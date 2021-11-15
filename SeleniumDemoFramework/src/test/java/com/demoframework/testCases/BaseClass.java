d300b9509a9b8e77174c9c24d493e599efc0cceb
ramrajnkl <getramraj@outlook.com>
2021-11-15 21:07:43 +0530

    Test run on different browser

--- SeleniumDemoFramework/src/test/java/com/demoframework/testCases/BaseClass.java
+++ SeleniumDemoFramework/src/test/java/com/demoframework/testCases/BaseClass.java
@@ -4,8 +4,11 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import com.demoframework.utilities.ReadConfig;
 
@@ -20,16 +23,32 @@ public class BaseClass {
 	public static WebDriver driver;
 	public  static Logger logger;
 	
+	@Parameters("browser")
 	@BeforeClass
-	public void setup()
+	public void setup(String br)
 	{	
-		
-		System.setProperty("webdriver.chrome.driver",  readconfig.getchromepath());
-		driver=new ChromeDriver();
 		
 		logger = Logger.getLogger("SeleniumDemoFramework");
 		PropertyConfigurator.configure("log4j.properties");
 		
+		if(br.equals("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver",  readconfig.getchromepath());
+			driver=new ChromeDriver();		
+			
+		}
+		else if (br.equals("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver",  readconfig.getfirefoxpath());
+			driver=new FirefoxDriver();	
+		}
+		else if (br.equals("ie"))
+		{
+			System.setProperty("webdriver.ie.driver",  readconfig.getiepath());
+			driver=new InternetExplorerDriver();	
+		}
+		
+		driver.get(baseURL);
 	}
 	
 	
